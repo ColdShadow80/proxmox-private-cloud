e74bf7d3e7a9eaee6310a0cb753ff620dd4b14c1
ColdShadow80 <46814978+ColdShadow80@users.noreply.github.com>
2026-03-08 11:03:22 +0000

    Add script to create and start LXC container
    Creates Debian 12 LXC with nesting enabled.

---
+++ scripts/03-create-lxc.sh
@@ -0,0 +1,13 @@
+CTID=$(cat /tmp/homelab_ctid)
+TEMPLATE=$(pveam available | grep debian-12 | head -n1 | awk '{print $2}')
+pveam download local $TEMPLATE
+
+pct create $CTID local:vztmpl/$TEMPLATE \
+  --hostname docker-host \
+  --cores 4 \
+  --memory 8192 \
+  --rootfs local-lvm:50 \
+  --features nesting=1,keyctl=1 \
+  --net0 name=eth0,bridge=vmbr0,ip=dhcp
+
+pct start $CTID
