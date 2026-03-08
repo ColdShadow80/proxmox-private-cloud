4622076ff04c708390e4ecdf7bd4412c15033eb8
ColdShadow80 <46814978+ColdShadow80@users.noreply.github.com>
2026-03-08 11:11:26 +0000

    Update GitHub repository URL in deployment script

--- scripts/06-deploy-gitops.sh
+++ scripts/06-deploy-gitops.sh
@@ -3,7 +3,7 @@ CTID=$(cat /tmp/homelab_ctid)
 pct exec $CTID -- bash -c "
 mkdir -p /opt/gitops
 cd /opt/gitops
-git clone https://github.com/YOURUSER/proxmox-private-cloud.git
+git clone https://github.com/ColdShadow80/proxmox-private-cloud.git
 cd proxmox-private-cloud/stacks
 docker compose -f homelab-stack.yml up -d
 "
